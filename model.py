13453bcde604828c833bf4b9dfb08f08d3a5a91c
haaaanung <hanung510@naver.com>
2025-10-13 17:12:25 +0900

    feat : regressor 모델 xgboost, randomforestregressor 추가

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
+from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 import lightgbm as lgb
 
@@ -13,9 +15,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 lgbm = lgb.LGBMRegressor(random_state=42)
 lgbm.fit(X_train, y_train)
+rf = RandomForestRegressor(random_state=42)
+rf.fit(X_train, y_train)
+xg = XGBRegressor(random_state=42)
+xg.fit(X_train, y_train)
 print('모델훈련완료')
 
-pred = lgbm.predict(X_test)
-
-rmse = np.sqrt(mean_squared_error(y_test, pred))
-print(f'예측 성능 RMSE : {rmse}')
+pred_lgbm = lgbm.predict(X_test)
+pred_rf = rf.predict(X_test)
+pred_xg = xg.predict(X_test)
+rmse_lgbm = np.sqrt(mean_squared_error(y_test, pred_lgbm))
+rmse_rf = np.sqrt(mean_squared_error(y_test, pred_rf))
+rmse_xg = np.sqrt(mean_squared_error(y_test, pred_xg))
+print(f'예측 성능 RMSE LightGBM:{rmse_lgbm}, RandomForest:{rmse_rf}, XGBoost:{rmse_xg}')
